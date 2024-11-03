5f36516a76baf5eda09638241ec35cae14020abd
fecym <yumingtarget@gmail.com>
2024-11-03 14:46:39 +0800

    feat: 初始化项目

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,21 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import moduleRoutes from "./importAll";
 
-const routes = [
+const constantRoutes = [
   {
     path: "/",
     name: "home",
     component: HomeView,
   },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  },
 ];
 
+console.log(import.meta.env.BASE_URL, "import.meta.env.BASE_URL");
+
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [...constantRoutes, ...moduleRoutes],
+  scrollBehavior: () => ({ left: 0, top: 0 }),
 });
 
 export default router;
